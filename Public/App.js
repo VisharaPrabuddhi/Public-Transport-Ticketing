db9b0ce1c020f0345c46ebcc34358f498f50e8f3
DiniruX <sandipadiniru@gmail.com>
2022-11-02 12:43:34 +0530

    few changes

--- Public/App.js
+++ Public/App.js
@@ -1,7 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native"
+
 import Home from "./screens/Home";
+import ChooseFrom from "./screens/Payment/ChooseFrom";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="ChooseFrom">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ChooseFrom" component={ChooseFrom} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
--- Public/screens/Home.js
+++ Public/screens/Home.js
@@ -5,21 +5,33 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text style={styles.signupText}>Hii Group members!</Text>
       <StatusBar style="auto" />
+      <Text style={styles.signupText}>Hii Group members!</Text>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   signupText: {
-    flex: 1,
-    color: "#ffffff",
+    color: "black",
     textAlign: "center",
   },
   container: {
     flex: 1,
-    backgroundColor: "#00704F",
-    
+    backgroundColor: "white", 
+    alignItems: 'center',
+    justifyContent: 'center',   
   },
 });
+
+{/* <View
+    style={{
+      flex: 1,
+      backgroundColor: '#000',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+    <Text style={{ color: '#fff' }}>Notice that the status bar has light text!</Text>
+    <StatusBar style="light" />
+  </View> */}
